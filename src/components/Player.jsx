7c88463bd38b062dc4b807122fe35176b3d510bf
Mohamed Nasr <mnasr0868@gmail.com>
2024-12-17 15:37:21 +0200

    first use of useRef hook and anderstanding the meaning of using it

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,19 @@
+import { useState, useRef } from "react";
 export default function Player() {
+  const [playerName, setPlayerName] = useState("");
+  const PlayerInput = useRef();
+  const handleClick = () => {
+    setPlayerName(PlayerInput.current.value);
+  };
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {playerName??'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input ref={PlayerInput} type="text" />
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
 }
+//if you want to read a value from an input field, you can use the useRef hook instead of using multiple states like the example above
+//know that that ref hook is actualy a pointer refer to any element has the property "ref" equals to the variable i store the useRef in so by the useRef hook i can access any property or method of the refered element easely accros the 'currunt' property of the useRef
