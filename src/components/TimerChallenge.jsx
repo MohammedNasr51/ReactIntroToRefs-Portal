8524082ed90b6c51b8c685be575e595a3d4ae6ed
Mohamed Nasr <mnasr0868@gmail.com>
2024-12-17 21:45:21 +0200

    adding the useRef hook to manage the value of the timer to close the timer and appling this functionality

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,13 +1,17 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function TimerChallenge({ title, targetTime }) {
+  const timer = useRef();
   const [timerExpired, setTimerExpierd] = useState(false);
   const [timerRunning, setTimerRunning] = useState(false);
   function handleStartChallenge() {
     setTimerRunning(true);
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       setTimerExpierd(true);
     }, targetTime * 1000);
+  }
+  function handleStopTimer() {
+    clearTimeout(timer.current);
   }
   return (
     <section className="challenge">
@@ -17,13 +21,17 @@ export default function TimerChallenge({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={handleStartChallenge}>
+        <button onClick={timerRunning ? handleStopTimer : handleStartChallenge}>
           {timerRunning ? "stop" : "start"} challenge
         </button>
       </p>
       <p className={timerRunning ? "active" : undefined}>
-        {timerRunning ? "time is running" : "time is up"}{" "}
+        {timerRunning ? "time is running" : "time is up"}
       </p>
     </section>
   );
 }
+
+// another use of useRef that if i have a value that i need to manage it in the component but i don't want to re-render the component when this value changes i can use the useRef hook to store this value and i can access it easely without re-rendering the component
+
+//knowing that if this value has a direct effect on the UI i should use the useState hook instead of the useRef hook
